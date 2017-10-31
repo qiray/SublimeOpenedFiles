f12e73132add1471257d353d281f1f4c55a03a81
qiray <zotovyaa@mail.ru>
2017-10-31 17:39:22 +0300

    Tree WIP

--- KateDocuments.py
+++ KateDocuments.py
@@ -1,7 +1,12 @@
 
+import os
+
 import sublime
 import sublime_plugin
 
+VERSION_MAJOR = 0
+VERSION_MINOR = 0
+VERSION_REVISION = 1
 
 class KateDocumentsCommand(sublime_plugin.TextCommand):
 	@staticmethod
@@ -15,5 +20,23 @@ class KateDocumentsCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		# self.view.insert(edit, 0, "Hello, World!")
 		view_list = self.view.window().views()
+		filenames = []
+		maxtree = []
 		for view in view_list:
-			self.view.insert(edit, 0, self.view_name(view) + '\n')
+			name = self.view_name(view)
+			print(name, '')
+			if name == '':
+				continue
+			arr = name.split(os.sep)
+			for i in range(0, len(arr) - 1):
+				if i >= len(maxtree):
+					maxtree.append(arr[i])
+					continue
+				if arr[i] != maxtree[i]:
+					maxtree = maxtree[:i]
+					break
+			filenames.append(arr)
+		print (maxtree, '')
+		for i in range(0, len(filenames)):
+			filenames[i] = filenames[i][len(maxtree):]
+		print (filenames, '')
