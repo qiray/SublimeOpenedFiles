17cff3ebcaa4b0dd6c4db1f46866aae72d08420f
qiray <zotovyaa@mail.ru>
2017-11-01 17:50:08 +0300

    First version tree is now done

--- KateDocuments.py
+++ KateDocuments.py
@@ -6,37 +6,63 @@ import sublime_plugin
 
 VERSION_MAJOR = 0
 VERSION_MINOR = 0
-VERSION_REVISION = 1
+VERSION_REVISION = 2
 
 class KateDocumentsCommand(sublime_plugin.TextCommand):
+
+	separator = '  '
+	untitled_name = 'untitled' #const
+	debug_level = 1
+
+	@staticmethod
+	def debug(level, *args):
+		if level <= KateDocumentsCommand.debug_level:
+			print('[DEBUG]', level, args)
+
 	@staticmethod
 	def view_name(view):
-		if view.file_name() is not None:
-			return view.file_name()
-		elif view.name() is not None:
-			return view.name()
-		return ''
+		result = KateDocumentsCommand.untitled_name
+		filename = view.file_name()
+		name = view.name()
+		if filename is not None and filename != '':
+			result = filename
+		elif name is not None and name != '':
+			result = name
+		return result
 
 	def run(self, edit):
 		# self.view.insert(edit, 0, "Hello, World!")
 		view_list = self.view.window().views()
 		filenames = []
 		maxtree = []
+		is_shorten = False
 		for view in view_list:
 			name = self.view_name(view)
-			print(name, '')
-			if name == '':
-				continue
+			self.debug(10, name)
 			arr = name.split(os.sep)
 			for i in range(0, len(arr) - 1):
 				if i >= len(maxtree):
+					if is_shorten:
+						continue
 					maxtree.append(arr[i])
 					continue
 				if arr[i] != maxtree[i]:
+					is_shorten = True
 					maxtree = maxtree[:i]
 					break
 			filenames.append(arr)
-		print (maxtree, '')
+		self.debug(1, maxtree)
 		for i in range(0, len(filenames)):
-			filenames[i] = filenames[i][len(maxtree):]
-		print (filenames, '')
+			if len(filenames[i]) > len(maxtree):
+				filenames[i] = filenames[i][len(maxtree):]
+		filenames = sorted(filenames)
+		self.debug(5, filenames)
+		result = ''
+		lasttree = []
+		for filename in filenames:
+			for i in range(0, len(filename)):
+				if len(lasttree) > i and lasttree[i] == filename[i]:
+					continue
+				result += self.separator*i + filename[i] + '\n'
+			lasttree = filename
+		print(result, '')
