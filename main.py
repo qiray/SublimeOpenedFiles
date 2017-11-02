0f064d4a98a6c929e6a084259c2a7488facc7f49
qiray <zotovyaa@mail.ru>
2017-11-02 18:05:59 +0300

    Use Sublime FileBrowser Plugin modules to show file tree in new tab

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import os
 
@@ -8,16 +10,22 @@ VERSION_MAJOR = 0
 VERSION_MINOR = 0
 VERSION_REVISION = 2
 
+ST3 = int(sublime.version()) >= 3000
+
+if ST3:
+    from .show import show
+else:  # ST2 imports
+    from show import show
+
+def debug(level, *args):
+	if level <= KateDocumentsCommand.debug_level:
+		print('[DEBUG]', level, args)
+
 class KateDocumentsCommand(sublime_plugin.TextCommand):
 
 	separator = '  '
 	untitled_name = 'untitled' #const
 	debug_level = 1
-
-	@staticmethod
-	def debug(level, *args):
-		if level <= KateDocumentsCommand.debug_level:
-			print('[DEBUG]', level, args)
 
 	@staticmethod
 	def view_name(view):
@@ -30,15 +38,13 @@ class KateDocumentsCommand(sublime_plugin.TextCommand):
 			result = name
 		return result
 
-	def run(self, edit):
-		# self.view.insert(edit, 0, "Hello, World!")
-		view_list = self.view.window().views()
+	def get_path(self, view_list):	
 		filenames = []
 		maxtree = []
 		is_shorten = False
 		for view in view_list:
 			name = self.view_name(view)
-			self.debug(10, name)
+			debug(10, name)
 			arr = name.split(os.sep)
 			for i in range(0, len(arr) - 1):
 				if i >= len(maxtree):
@@ -51,12 +57,12 @@ class KateDocumentsCommand(sublime_plugin.TextCommand):
 					maxtree = maxtree[:i]
 					break
 			filenames.append(arr)
-		self.debug(1, maxtree)
+		debug(1, maxtree)
 		for i in range(0, len(filenames)):
 			if len(filenames[i]) > len(maxtree):
 				filenames[i] = filenames[i][len(maxtree):]
 		filenames = sorted(filenames)
-		self.debug(5, filenames)
+		debug(5, filenames)
 		result = ''
 		lasttree = []
 		for filename in filenames:
@@ -65,4 +71,16 @@ class KateDocumentsCommand(sublime_plugin.TextCommand):
 					continue
 				result += self.separator*i + filename[i] + '\n'
 			lasttree = filename
-		print(result, '')
+		return result, maxtree, filenames
+
+	def run(self, edit):
+		# self.view.insert(edit, 0, "Hello, World!")
+		window = self.view.window()
+		view_list = window.views()
+		result, maxtree, filenames = self.get_path(view_list)
+
+		#From ST FileBrowser
+		view = show(window, os.sep . join(maxtree), other_group=True)
+		view.erase(edit, sublime.Region(0, view.size())) #clear view content
+		view.insert(edit, 0, result) #paste result string
+		window.focus_view(view)
