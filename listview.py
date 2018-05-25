7587f12a069dee58c9a9c4b7757822e135be39d8
qiray <zotovyaa@mail.ru>
2018-05-25 13:15:09 +0300

    Revert "treeview - windows finished"
    This reverts commit 53fc4681e36d904e991366f0cd31ad562d716f99.

--- listview.py
+++ listview.py
@@ -8,10 +8,9 @@ ST3 = int(sublime.version()) >= 3000
 
 class Element(object):
     """This is an object representing view storing in listview"""
-    def __init__(self, name, node_id, window):
+    def __init__(self, name, node_id):
         self.node_id = node_id
         self.name = name
-        self.window = window
         self.stringnum = 0
 
     def get_name(self):
@@ -23,7 +22,7 @@ class List(object):
     def __init__(self):
         self.views = {}
 
-    def add_filename(self, filename, view_id, is_file, window):
+    def add_filename(self, filename, view_id, is_file):
         """
         Function to add view into the list
         is_file param is used to differ opened files from nonfile views
@@ -32,38 +31,23 @@ class List(object):
         name = arr[-1]
         if not is_file:
             newname = '{} (id = {})'.format(filename, view_id)
-            self.views[newname] = Element(newname, view_id, window)
+            self.views[newname] = Element(newname, view_id)
             return
-        self.views[name] = Element(name, view_id, window)
+        self.views[name] = Element(name, view_id)
 
     def __str__(self):
         """Function to draw listview"""
         result = ''
         templist = sorted(self.views)
-        templist.sort(key=lambda x: self.views[x].window.id())
         stringnum = 1
-        win_id = 0
-        prev_id = 1
         for node in templist:
-            win_id = self.views[node].window.id()
-            if win_id != prev_id:
-                result += "Hm.\n"
-                stringnum += 1
-            prev_id = win_id
             result += '≡ ' + self.views[node].name + "\n"
             self.views[node].stringnum = stringnum
             stringnum += 1
         return result
 
-    def get_action(self, stringnum):
-        """Get action by string number"""
-        for elm in self.views:
-            if self.views[elm].stringnum == stringnum:
-                return self.views[elm].node_id
-        return None
-
     def get_view_id(self, stringnum):
-        """Get action by string number"""
+        """Get view id by string number"""
         for elm in self.views:
             if self.views[elm].stringnum == stringnum:
                 return self.views[elm].node_id
